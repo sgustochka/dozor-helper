3b887fc2237acc7dcc3064485e63fbdbf8281e71
sgustochka <postovaya7@gmail.com>
2026-04-04 19:23:50 +0500

    dozor-helper.js

--- Дозорный помощник Ветра-1.0.user.js
+++ Дозорный помощник Ветра-1.0.user.js
@@ -10,6 +10,9 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @run-at       document-idle
+// @updateURL    https://github.com/sgustochka/dozor-helper/blob/main/%D0%94%D0%BE%D0%B7%D0%BE%D1%80%D0%BD%D1%8B%D0%B9%20%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D0%BD%D0%B8%D0%BA%20%D0%92%D0%B5%D1%82%D1%80%D0%B0-1.0.user.js
+// @downloadURL  https://github.com/sgustochka/dozor-helper/blob/main/%D0%94%D0%BE%D0%B7%D0%BE%D1%80%D0%BD%D1%8B%D0%B9%20%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D0%BD%D0%B8%D0%BA%20%D0%92%D0%B5%D1%82%D1%80%D0%B0-1.0.user.js
+// ==/UserScript==
 // ==/UserScript==
 
 (function() {
